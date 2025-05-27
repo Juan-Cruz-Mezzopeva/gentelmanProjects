ba6500d509d646e799ddbfa89feb74cd8e46b513
Juan-Cruz-Mezzopeva <111082625+Juan-Cruz-Mezzopeva@users.noreply.github.com>
2025-05-27 18:24:25 -0300

    codigo de uso de useEffect

--- react-by-gentelman-programing/src/App.tsx
+++ react-by-gentelman-programing/src/App.tsx
@@ -1,49 +1,44 @@
-import { useState } from 'react'
-import { Button } from './Components'
+import { useEffect, useState } from 'react'
 import './App.css'
 
-
-
 function App() {
-  
-const [count, setCount] = useState(0)
+  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState("")
 
-const [countB, setCountB] = useState(0)
+  const fetchData = async () => {
+    setLoading(true)
+    try {
+      const response = await fetch("https://api.example.com/data")
 
-// en este caso setCountB se ejecuta 5 veces y se renderiza una vez
-// pero en cada llamada a setCountB  se ejecuta la función que le pasamos
-// por lo que el valor final de countB es 5, esto es porque cada setCountB
-// recibe el valor actual de countB y le suma 1, y aca si va a sumar de 5
-// porque el valor de countB es pasado por parametro en cada ejecucion de 
-// la funcion flecha que contiene  setCountB, por lo que al final
-// countB es 5, ya que se ha ejecutado 5 veces la función que le pasamos
+      if (!response.ok) {
+        throw new Error("Error al obtener datos")
+      }
 
-const countMoreB = () => {
-  setCountB((countB) => countB + 1)
-  setCountB((countB) => countB + 1)
-  setCountB((countB) => countB + 1)
-  setCountB((countB) => countB + 1)
-  setCountB((countB) => countB + 1)
-}
+      const jsonData = await response.json()
 
-// en este caso setCount se ejecuta 5 veces y se renderiza una vez
-// pero en cada llamada a setCount count esta tomando el valor de el render actual
-// que al no re-renderizarse en cada llamada, siempre toma el valor cero y 
-// le suma 1 por lo que su valor final va a ser 1
-const countMore = () => {
-  setCount(count + 1)
-  setCount(count + 1)
-  setCount(count + 1)
-  setCount(count + 1)
-  setCount(count + 1)
-}
+      setData(jsonData)
+    } catch (err) {
+      setError(err as string)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  if (loading) {
+    return <div>Cargando...</div>
+  }
+
+  if (error) {
+    return <div>UPS! Hay un error: {error}</div>
+  }
 
   return (
-    <>
-       <Button label={`Count is ${count}`} parentMethod={countMore}/>     
-       <Button label={`Count B is ${countB}`} parentMethod={countMoreB}/>     
-
-    </>
+    <div>{JSON.stringify(data)}</div>
   )
 }
 
